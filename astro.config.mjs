c21d5c538e2397f48d8282e774c28a772dde5f3d
Sgal Cheung <zhangsigao@live.com>
2025-05-22 14:35:16 +0800

    feat: update Astro configuration and enhance header component
    - Added passthrough image service to Astro configuration for improved image handling.
    - Updated Header component to include a new internal link and improved formatting.
    - Introduced new SVG assets for branding and background.
    - Created a Welcome component to serve as a landing section with background and logo.
    - Updated hello.astro page to utilize the new Welcome component for a better user experience.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
@@ -9,7 +9,11 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
+  image: {
+    service: passthroughImageService(),
+  },
   adapter: cloudflare({
+    imageService: "passthrough",
     platformProxy: {
       enabled: true,
     },
